ddabfa33a1efc5472d00ac0b3c2953e126bdea07
Kathryn Fieg <kathrynfieg@gmail.com>
2024-11-18 22:53:55 +1030

    chore: install and setup Vuetify

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import HelloWorld from './components/HelloWorld.vue';
 </script>
 
-<template><div class="bg-slate-400 border border-black">APP</div></template>
+<template>
+  <div class="bg-slate-400 border border-black"><v-btn> Button </v-btn></div>
+</template>
 
 <style scoped></style>
